373253040294ed866de4cc9262671bedca5d6e65
Keywan Ghadami <keywan.ghadami@gmail.com>
2026-01-18 21:45:36 +0100

    Update model.rs

--- src/model.rs
+++ src/model.rs
@@ -1,14 +1,15 @@
 use syn::{Ident, Type, LitStr, Lit};
 use proc_macro2::{TokenStream, Span};
+use syn_grammar_macros::ModelConvert;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, ModelConvert)]
 pub struct GrammarDefinition {
     pub name: Ident,
     pub inherits: Option<Ident>, 
     pub rules: Vec<Rule>,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, ModelConvert)]
 pub struct Rule {
     pub is_pub: bool, 
     pub name: Ident,
@@ -16,7 +17,7 @@ pub struct Rule {
     pub variants: Vec<RuleVariant>,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, ModelConvert)]
 pub struct RuleVariant {
     pub pattern: Vec<Pattern>,
     pub action: TokenStream,
@@ -39,21 +40,31 @@ pub enum Pattern {
     Plus(Box<Pattern>),
 }
 
+// Manuelle Konvertierung für Enums (da ModelConvert nur Structs kann)
+impl From<crate::parser::Pattern> for Pattern {
+    fn from(p: crate::parser::Pattern) -> Self {
+        match p {
+            crate::parser::Pattern::Lit(l) => Pattern::Lit(l),
+            crate::parser::Pattern::RuleCall { binding, rule_name, args } => 
+                Pattern::RuleCall { binding, rule_name, args },
+            crate::parser::Pattern::Group(alts) => 
+                Pattern::Group(alts.into_iter().map(|seq| seq.into_iter().map(Into::into).collect()).collect()),
+            crate::parser::Pattern::Bracketed(p) => Pattern::Bracketed(p.into_iter().map(Into::into).collect()),
+            crate::parser::Pattern::Braced(p) => Pattern::Braced(p.into_iter().map(Into::into).collect()),
+            crate::parser::Pattern::Parenthesized(p) => Pattern::Parenthesized(p.into_iter().map(Into::into).collect()),
+            crate::parser::Pattern::Optional(p) => Pattern::Optional(Box::new((*p).into())),
+            crate::parser::Pattern::Repeat(p) => Pattern::Repeat(Box::new((*p).into())),
+            crate::parser::Pattern::Plus(p) => Pattern::Plus(Box::new((*p).into())),
+        }
+    }
+}
+
 impl Pattern {
     pub fn span(&self) -> Span {
         match self {
             Pattern::Lit(l) => syn::spanned::Spanned::span(l),
             Pattern::RuleCall { rule_name, .. } => syn::spanned::Spanned::span(rule_name),
-            Pattern::Group(alts) => {
-                alts.first()
-                    .and_then(|seq| seq.first())
-                    .map(|p| p.span())
-                    .unwrap_or_else(Span::call_site)
-            },
-            Pattern::Bracketed(seq) | Pattern::Braced(seq) | Pattern::Parenthesized(seq) => {
-                 seq.first().map(|p| p.span()).unwrap_or_else(Span::call_site)
-            },
-            Pattern::Optional(p) | Pattern::Repeat(p) | Pattern::Plus(p) => p.span(),
+            _ => Span::call_site(),
         }
     }
 }
