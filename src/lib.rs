02648c6896349b9b6476673305cb9acd3a622eba
Keywan Ghadami <keywan.ghadami@gmail.com>
2026-01-19 15:47:06 +0100

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@
 // Der vom Makro generierte Code verweist auf `syn_grammar::rt`.
 // Daher muss dieses Modul öffentlich verfügbar sein.
 pub mod rt;
-
+pub mod testing;
 // 3. Das Makro re-exportieren
 // Damit kann der User schreiben: `use syn_grammar::grammar;`
 pub use syn_grammar_macros::grammar;
