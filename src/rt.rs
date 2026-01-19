1bcfdfe75345fd1b3ce2c13ab8271dc9ed0602eb
Keywan Ghadami <keywan.ghadami@gmail.com>
2026-01-19 04:34:25 +0100

    Update rt.rs

--- src/rt.rs
+++ src/rt.rs
@@ -1,13 +1,19 @@
-use syn::parse::{ParseStream};
+use syn::parse::ParseStream;
 use syn::Result;
+
+// 1. WICHTIG für Backtracking: Aktiviert .fork() und .advance_to()
+use syn::parse::discouraged::Speculative;
+
+// 2. WICHTIG für Identifier: Aktiviert .parse_any() (erlaubt Keywords als Namen)
+use syn::ext::IdentExt; 
 
 /// Kapselt einen spekulativen Parse-Versuch. 
 /// Erlaubt es dem Generator, Backtracking als Einzeiler zu schreiben.
 pub fn attempt<T>(input: ParseStream, parser: impl FnOnce(ParseStream) -> Result<T>) -> Result<Option<T>> {
-    let fork = input.fork();
+    let fork = input.fork(); // Benötigt 'Speculative'
     match parser(&fork) {
         Ok(res) => {
-            input.advance_to(&fork);
+            input.advance_to(&fork); // Benötigt 'Speculative'
             Ok(Some(res))
         }
         Err(_) => Ok(None),
@@ -16,6 +22,7 @@ pub fn attempt<T>(input: ParseStream, parser: impl FnOnce(ParseStream) -> Result
 
 /// Helper für Identifier (erlaubt Keywords)
 pub fn parse_ident(input: ParseStream) -> Result<syn::Ident> {
+    // Benötigt 'IdentExt'
     input.call(syn::Ident::parse_any)
 }
 
