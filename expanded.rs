d934218f7bd48f14f5875c19a39b00c5d561c928
keywan.ghadami <keywan.ghadami@gmail.com>
2026-03-21 10:11:35 +0000

    expanded

--- expanded.rs
+++ expanded.rs
@@ -1,71 +1,45 @@
-    Checking winnow-grammar v0.1.0 (/home/user/syn-grammar/winnow-grammar)
-    Finished `dev` profile [unoptimized + debuginfo] target(s) in 5.88s
-
 #![feature(prelude_import)]
 #[macro_use]
 extern crate std;
 #[prelude_import]
 use std::prelude::rust_2021::*;
+use winnow::stream::LocatingSlice;
+use winnow::Parser;
 use winnow_grammar::grammar;
-use winnow_grammar::testing::WinnowTestExt;
-pub enum Expr {
-    Num(u32),
-    Add(Box<Expr>, Box<Expr>),
+pub struct CustomNode {
+    pub name: String,
+    pub span: std::ops::Range<usize>,
 }
 #[automatically_derived]
-impl ::core::fmt::Debug for Expr {
+impl ::core::fmt::Debug for CustomNode {
     #[inline]
     fn fmt(&self, f: &mut ::core::fmt::Formatter) -> ::core::fmt::Result {
-        match self {
-            Expr::Num(__self_0) => {
-                ::core::fmt::Formatter::debug_tuple_field1_finish(f, "Num", &__self_0)
-            }
-            Expr::Add(__self_0, __self_1) => {
-                ::core::fmt::Formatter::debug_tuple_field2_finish(
-                    f,
-                    "Add",
-                    __self_0,
-                    &__self_1,
-                )
-            }
-        }
+        ::core::fmt::Formatter::debug_struct_field2_finish(
+            f,
+            "CustomNode",
+            "name",
+            &self.name,
+            "span",
+            &&self.span,
+        )
     }
 }
 #[automatically_derived]
-impl ::core::marker::StructuralPartialEq for Expr {}
+impl ::core::marker::StructuralPartialEq for CustomNode {}
 #[automatically_derived]
-impl ::core::cmp::PartialEq for Expr {
+impl ::core::cmp::PartialEq for CustomNode {
     #[inline]
-    fn eq(&self, other: &Expr) -> bool {
-        let __self_discr = ::core::intrinsics::discriminant_value(self);
-        let __arg1_discr = ::core::intrinsics::discriminant_value(other);
-        __self_discr == __arg1_discr
-            && match (self, other) {
-                (Expr::Num(__self_0), Expr::Num(__arg1_0)) => __self_0 == __arg1_0,
-                (Expr::Add(__self_0, __self_1), Expr::Add(__arg1_0, __arg1_1)) => {
-                    __self_0 == __arg1_0 && __self_1 == __arg1_1
-                }
-                _ => unsafe { ::core::intrinsics::unreachable() }
-            }
+    fn eq(&self, other: &CustomNode) -> bool {
+        self.name == other.name && self.span == other.span
     }
 }
-#[automatically_derived]
-impl ::core::clone::Clone for Expr {
-    #[inline]
-    fn clone(&self) -> Expr {
-        match self {
-            Expr::Num(__self_0) => Expr::Num(::core::clone::Clone::clone(__self_0)),
-            Expr::Add(__self_0, __self_1) => {
-                Expr::Add(
-                    ::core::clone::Clone::clone(__self_0),
-                    ::core::clone::Clone::clone(__self_1),
-                )
-            }
-        }
+impl CustomNode {
+    fn from_data(name: String, span: std::ops::Range<usize>) -> Self {
+        Self { name, span }
     }
 }
 #[allow(non_snake_case)]
-pub mod LeftRec {
+pub mod ExplicitSpanTest {
     #![allow(unused_imports)]
     #![allow(dead_code)]
     use super::*;
@@ -77,158 +51,145 @@ pub mod LeftRec {
         input: &mut ::winnow_grammar::ParseInput<'a, S>,
     ) -> ::winnow::Result<
         (),
-        ::winnow::error::InputError<::winnow_grammar::ParseInput<'a, S>>,
+        ::winnow::error::ErrMode<
+            ::winnow::error::InputError<::winnow_grammar::ParseInput<'a, S>>,
+        >,
     > {
         use ::winnow::Parser;
         ::winnow::ascii::multispace0.parse_next(input).map(|_| ())
     }
     #[allow(dead_code)]
-    fn parse_expr_inner<'a, S: std::fmt::Debug + Clone>(
+    fn parse_custom_node_inner<'a, S: std::fmt::Debug + Clone>(
         input: &mut ::winnow_grammar::ParseInput<'a, S>,
     ) -> ::winnow::Result<
-        Expr,
-        ::winnow::error::InputError<::winnow_grammar::ParseInput<'a, S>>,
+        CustomNode,
+        ::winnow::error::ErrMode<
+            ::winnow::error::InputError<::winnow_grammar::ParseInput<'a, S>>,
+        >,
     > {
         use ::winnow::Parser;
         let mut parser = (|
             input: &mut ::winnow_grammar::ParseInput<'a, S>,
         | -> ::winnow::Result<
-            Expr,
-            ::winnow::error::InputError<::winnow_grammar::ParseInput<'a, S>>,
-        > {
-            use ::winnow::prelude::*;
-            let mut lhs = {
-                let _ = WS(input)?;
-                let t = (|i: &mut ::winnow_grammar::ParseInput<'a, S>| parse_term_inner(
-                    i,
-                ))
-                    .parse_next(input)?;
-                Ok(t)
-            }?;
-            loop {
-                {
-                    let checkpoint = ::winnow::stream::Stream::checkpoint(input);
-                    let attempt = (|| {
-                        let _ = WS(input)?;
-                        let _ = literal("+")
-                            .context(
-                                ::winnow::error::StrContext::Expected(
-                                    ::winnow::error::StrContextValue::StringLiteral("+"),
-                                ),
-                            )
-                            .parse_next(input)?;
-                        let _ = WS(input)?;
-                        let r = (|i: &mut ::winnow_grammar::ParseInput<'a, S>| parse_term_inner(
-                            i,
-                        ))
-                            .parse_next(input)?;
-                        let l = lhs.clone();
-                        Ok(Expr::Add(Box::new(l), Box::new(r)))
-                    })();
-                    match attempt {
-                        Ok(val) => {
-                            lhs = val;
-                            continue;
-                        }
-                        Err(e) => {
-                            if #[allow(non_exhaustive_omitted_patterns)]
-                            match e {
-                                ::winnow::error::ErrMode::Cut(_) => true,
-                                _ => false,
-                            } {
-                                return Err(e);
-                            } else {
-                                ::winnow::stream::Stream::reset(input, &checkpoint);
-                            }
-                        }
-                    }
-                }
-                break;
-            }
-            Ok(lhs)
-        })
-            .context(::winnow::error::StrContext::Label("expr"));
-        { parser.parse_next(input) }
-    }
-    pub fn parse_expr<'a, S: std::fmt::Debug + Clone>() -> impl ::winnow::Parser<
-        ::winnow_grammar::ParseInput<'a, S>,
-        Expr,
-        ::winnow::error::InputError<::winnow_grammar::ParseInput<'a, S>>,
-    > {
-        move |
-            input: &mut ::winnow_grammar::ParseInput<'a, S>,
-        | -> ::winnow::Result<
-            Expr,
-            ::winnow::error::InputError<::winnow_grammar::ParseInput<'a, S>>,
-        > {
-            let _ = WS(input)?;
-            let result = parse_expr_inner(input)?;
-            let _ = WS(input)?;
-            ::winnow::combinator::eof.parse_next(input)?;
-            Ok(result)
-        }
-    }
-    #[allow(dead_code)]
-    fn parse_term_inner<'a, S: std::fmt::Debug + Clone>(
-        input: &mut ::winnow_grammar::ParseInput<'a, S>,
-    ) -> ::winnow::Result<
-        Expr,
-        ::winnow::error::InputError<::winnow_grammar::ParseInput<'a, S>>,
-    > {
-        use ::winnow::Parser;
-        let mut parser = (|
-            input: &mut ::winnow_grammar::ParseInput<'a, S>,
-        | -> ::winnow::Result<
-            Expr,
-            ::winnow::error::InputError<::winnow_grammar::ParseInput<'a, S>>,
+            CustomNode,
+            ::winnow::error::ErrMode<
+                ::winnow::error::InputError<::winnow_grammar::ParseInput<'a, S>>,
+            >,
         > {
             use ::winnow::prelude::*;
             {
                 let _ = WS(input)?;
-                let n = ::winnow::ascii::dec_uint::<
-                    ::winnow_grammar::ParseInput<'a, S>,
-                    u32,
-                    ::winnow::error::InputError<::winnow_grammar::ParseInput<'a, S>>,
-                >
+                let start = ::winnow::stream::Location::location(input);
+                let a = ::winnow::token::take_while(
+                        1..,
+                        |c| {
+                            ::winnow::stream::AsChar::as_char(c).is_alphanumeric()
+                                || ::winnow::stream::AsChar::as_char(c) == '_'
+                        },
+                    )
                     .parse_next(input)?;
-                Ok(Expr::Num(n))
+                let end = ::winnow::stream::Location::location(input);
+                #[allow(unused_variables)]
+                let _span = start..end;
+                Ok(CustomNode::from_data(a.to_string(), _span))
             }
         })
-            .context(::winnow::error::StrContext::Label("term"));
+            .context(::winnow::error::StrContext::Label("custom_node"));
         { parser.parse_next(input) }
     }
-    fn parse_term<'a, S: std::fmt::Debug + Clone>() -> impl ::winnow::Parser<
+    pub fn parse_custom_node<'a, S: std::fmt::Debug + Clone>() -> impl ::winnow::Parser<
         ::winnow_grammar::ParseInput<'a, S>,
-        Expr,
+        CustomNode,
         ::winnow::error::InputError<::winnow_grammar::ParseInput<'a, S>>,
     > {
         move |
             input: &mut ::winnow_grammar::ParseInput<'a, S>,
         | -> ::winnow::Result<
-            Expr,
+            CustomNode,
             ::winnow::error::InputError<::winnow_grammar::ParseInput<'a, S>>,
         > {
-            let _ = WS(input)?;
-            let result = parse_term_inner(input)?;
-            let _ = WS(input)?;
-            ::winnow::combinator::eof.parse_next(input)?;
+            use ::winnow::error::ParserError;
+            match WS(input) {
+                Ok(v) => v,
+                Err(
+                    ::winnow::error::ErrMode::Backtrack(err)
+                    | ::winnow::error::ErrMode::Cut(err),
+                ) => return Err(err),
+                Err(::winnow::error::ErrMode::Incomplete(needed)) => {
+                    return Err(
+                        <::winnow::error::InputError<
+                            ::winnow_grammar::ParseInput<'a, S>,
+                        > as ParserError<
+                            ::winnow_grammar::ParseInput<'a, S>,
+                        >>::incomplete(input, needed),
+                    );
+                }
+            };
+            let result = match parse_custom_node_inner(input) {
+                Ok(v) => v,
+                Err(
+                    ::winnow::error::ErrMode::Backtrack(err)
+                    | ::winnow::error::ErrMode::Cut(err),
+                ) => return Err(err),
+                Err(::winnow::error::ErrMode::Incomplete(needed)) => {
+                    return Err(
+                        <::winnow::error::InputError<
+                            ::winnow_grammar::ParseInput<'a, S>,
+                        > as ParserError<
+                            ::winnow_grammar::ParseInput<'a, S>,
+                        >>::incomplete(input, needed),
+                    );
+                }
+            };
+            match WS(input) {
+                Ok(v) => v,
+                Err(
+                    ::winnow::error::ErrMode::Backtrack(err)
+                    | ::winnow::error::ErrMode::Cut(err),
+                ) => return Err(err),
+                Err(::winnow::error::ErrMode::Incomplete(needed)) => {
+                    return Err(
+                        <::winnow::error::InputError<
+                            ::winnow_grammar::ParseInput<'a, S>,
+                        > as ParserError<
+                            ::winnow_grammar::ParseInput<'a, S>,
+                        >>::incomplete(input, needed),
+                    );
+                }
+            };
+            match ::winnow::combinator::eof.parse_next(input) {
+                Ok(v) => v,
+                Err(
+                    ::winnow::error::ErrMode::Backtrack(err)
+                    | ::winnow::error::ErrMode::Cut(err),
+                ) => return Err(err),
+                Err(::winnow::error::ErrMode::Incomplete(needed)) => {
+                    return Err(
+                        <::winnow::error::InputError<
+                            ::winnow_grammar::ParseInput<'a, S>,
+                        > as ParserError<
+                            ::winnow_grammar::ParseInput<'a, S>,
+                        >>::incomplete(input, needed),
+                    );
+                }
+            };
             Ok(result)
         }
     }
 }
 extern crate test;
-#[rustc_test_marker = "test_left_recursion"]
+#[rustc_test_marker = "test_explicit_span_injection"]
 #[doc(hidden)]
-pub const test_left_recursion: test::TestDescAndFn = test::TestDescAndFn {
+pub const test_explicit_span_injection: test::TestDescAndFn = test::TestDescAndFn {
     desc: test::TestDesc {
-        name: test::StaticTestName("test_left_recursion"),
+        name: test::StaticTestName("test_explicit_span_injection"),
         ignore: false,
         ignore_message: ::core::option::Option::None,
-        source_file: "winnow-grammar/tests/left_recursion.rs",
-        start_line: 22usize,
+        source_file: "winnow-grammar/tests/explicit_span_test.rs",
+        start_line: 24usize,
         start_col: 4usize,
-        end_line: 22usize,
-        end_col: 23usize,
+        end_line: 24usize,
+        end_col: 32usize,
         compile_fail: false,
         no_run: false,
         should_panic: test::ShouldPanic::No,
@@ -236,23 +197,44 @@ pub const test_left_recursion: test::TestDescAndFn = test::TestDescAndFn {
     },
     testfn: test::StaticTestFn(
         #[coverage(off)]
-        || test::assert_test_result(test_left_recursion()),
+        || test::assert_test_result(test_explicit_span_injection()),
     ),
 };
-fn test_left_recursion() {
-    LeftRec::parse_expr()
-        .parse_test("1 + 2 + 3")
-        .assert_success_is(
-            Expr::Add(
-                Box::new(Expr::Add(Box::new(Expr::Num(1)), Box::new(Expr::Num(2)))),
-                Box::new(Expr::Num(3)),
-            ),
-        );
+fn test_explicit_span_injection() {
+    let input = "  my_ident  ";
+    let input = LocatingSlice::new(input);
+    let result = ExplicitSpanTest::parse_custom_node().parse(input).unwrap();
+    match (&result.name, &"my_ident") {
+        (left_val, right_val) => {
+            if !(*left_val == *right_val) {
+                let kind = ::core::panicking::AssertKind::Eq;
+                ::core::panicking::assert_failed(
+                    kind,
+                    &*left_val,
+                    &*right_val,
+                    ::core::option::Option::None,
+                );
+            }
+        }
+    };
+    match (&result.span, &(2..10)) {
+        (left_val, right_val) => {
+            if !(*left_val == *right_val) {
+                let kind = ::core::panicking::AssertKind::Eq;
+                ::core::panicking::assert_failed(
+                    kind,
+                    &*left_val,
+                    &*right_val,
+                    ::core::option::Option::None,
+                );
+            }
+        }
+    };
 }
 #[rustc_main]
 #[coverage(off)]
 #[doc(hidden)]
 pub fn main() -> () {
     extern crate test;
-    test::test_main_static(&[&test_left_recursion])
+    test::test_main_static(&[&test_explicit_span_injection])
 }
