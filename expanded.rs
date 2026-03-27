395dda76a18d9245a9e492f8f181cc5c05686f45
keywan.ghadami <keywan.ghadami@gmail.com>
2026-03-27 07:05:07 +0000

    wip depug span test

--- expanded.rs
+++ expanded.rs
@@ -1,48 +1,55 @@
-    Checking winnow-grammar v0.1.0 (/home/user/syn-grammar/winnow-grammar)
-    Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.25s
-
 #![feature(prelude_import)]
 #[macro_use]
 extern crate std;
 #[prelude_import]
 use std::prelude::rust_2021::*;
-use winnow::stream::LocatingSlice;
-use winnow::Parser;
+use grammar_kit::WithSpan;
 use winnow_grammar::grammar;
-pub struct CustomNode {
-    pub name: String,
+use winnow_grammar::testing::WinnowTestExt;
+pub struct SpannedInt {
+    pub val: u32,
     pub span: std::ops::Range<usize>,
 }
 #[automatically_derived]
-impl ::core::fmt::Debug for CustomNode {
+impl ::core::fmt::Debug for SpannedInt {
     #[inline]
     fn fmt(&self, f: &mut ::core::fmt::Formatter) -> ::core::fmt::Result {
         ::core::fmt::Formatter::debug_struct_field2_finish(
             f,
-            "CustomNode",
-            "name",
-            &self.name,
+            "SpannedInt",
+            "val",
+            &self.val,
             "span",
             &&self.span,
         )
     }
 }
 #[automatically_derived]
-impl ::core::marker::StructuralPartialEq for CustomNode {}
+impl ::core::marker::StructuralPartialEq for SpannedInt {}
 #[automatically_derived]
-impl ::core::cmp::PartialEq for CustomNode {
+impl ::core::cmp::PartialEq for SpannedInt {
     #[inline]
-    fn eq(&self, other: &CustomNode) -> bool {
-        self.name == other.name && self.span == other.span
+    fn eq(&self, other: &SpannedInt) -> bool {
+        self.val == other.val && self.span == other.span
     }
 }
-impl CustomNode {
-    fn from_data(name: String, span: std::ops::Range<usize>) -> Self {
-        Self { name, span }
+#[automatically_derived]
+impl ::core::clone::Clone for SpannedInt {
+    #[inline]
+    fn clone(&self) -> SpannedInt {
+        SpannedInt {
+            val: ::core::clone::Clone::clone(&self.val),
+            span: ::core::clone::Clone::clone(&self.span),
+        }
+    }
+}
+impl WithSpan<u32> for SpannedInt {
+    fn with_span(val: u32, span: std::ops::Range<usize>) -> Self {
+        Self { val, span }
     }
 }
 #[allow(non_snake_case)]
-pub mod ExplicitSpanTest {
+pub mod SpanTest {
     #![allow(unused_imports)]
     #![allow(dead_code)]
     use super::*;
@@ -62,10 +69,10 @@ pub mod ExplicitSpanTest {
         ::winnow::ascii::multispace0.parse_next(input).map(|_| ())
     }
     #[allow(dead_code)]
-    fn parse_custom_node_inner<'a, S: std::fmt::Debug + Clone>(
+    fn parse_main_inner<'a, S: std::fmt::Debug + Clone>(
         input: &mut ::winnow_grammar::ParseInput<'a, S>,
     ) -> ::winnow::Result<
-        CustomNode,
+        SpannedInt,
         ::winnow::error::ErrMode<
             ::winnow::error::InputError<::winnow_grammar::ParseInput<'a, S>>,
         >,
@@ -74,7 +81,7 @@ pub mod ExplicitSpanTest {
         let mut parser = (|
             input: &mut ::winnow_grammar::ParseInput<'a, S>,
         | -> ::winnow::Result<
-            CustomNode,
+            SpannedInt,
             ::winnow::error::ErrMode<
                 ::winnow::error::InputError<::winnow_grammar::ParseInput<'a, S>>,
             >,
@@ -83,32 +90,32 @@ pub mod ExplicitSpanTest {
             {
                 let _ = WS(input)?;
                 let start = ::winnow::stream::Location::location(input);
-                let a = ::winnow::token::take_while(
-                        1..,
-                        |c| {
-                            ::winnow::stream::AsChar::as_char(c).is_alphanumeric()
-                                || ::winnow::stream::AsChar::as_char(c) == '_'
-                        },
-                    )
+                let n = ::winnow::ascii::dec_uint::<
+                    ::winnow_grammar::ParseInput<'a, S>,
+                    u32,
+                    ::winnow::error::ErrMode<
+                        ::winnow::error::InputError<::winnow_grammar::ParseInput<'a, S>>,
+                    >,
+                >
                     .parse_next(input)?;
                 let end = ::winnow::stream::Location::location(input);
                 #[allow(unused_variables)]
                 let _span = start..end;
-                Ok(CustomNode::from_data(a.to_string(), _span))
+                Ok(::grammar_kit::WithSpan::with_span({ n }, _span))
             }
         })
-            .context(::winnow::error::StrContext::Label("custom_node"));
+            .context(::winnow::error::StrContext::Label("main"));
         { parser.parse_next(input) }
     }
-    pub fn parse_custom_node<'a, S: std::fmt::Debug + Clone>() -> impl ::winnow::Parser<
+    pub fn parse_main<'a, S: std::fmt::Debug + Clone>() -> impl ::winnow::Parser<
         ::winnow_grammar::ParseInput<'a, S>,
-        CustomNode,
+        SpannedInt,
         ::winnow::error::InputError<::winnow_grammar::ParseInput<'a, S>>,
     > {
         move |
             input: &mut ::winnow_grammar::ParseInput<'a, S>,
         | -> ::winnow::Result<
-            CustomNode,
+            SpannedInt,
             ::winnow::error::InputError<::winnow_grammar::ParseInput<'a, S>>,
         > {
             use ::winnow::error::ParserError;
@@ -128,7 +135,7 @@ pub mod ExplicitSpanTest {
                     );
                 }
             };
-            let result = match parse_custom_node_inner(input) {
+            let result = match parse_main_inner(input) {
                 Ok(v) => v,
                 Err(
                     ::winnow::error::ErrMode::Backtrack(err)
@@ -181,18 +188,18 @@ pub mod ExplicitSpanTest {
     }
 }
 extern crate test;
-#[rustc_test_marker = "test_explicit_span_injection"]
+#[rustc_test_marker = "test_span_injection"]
 #[doc(hidden)]
-pub const test_explicit_span_injection: test::TestDescAndFn = test::TestDescAndFn {
+pub const test_span_injection: test::TestDescAndFn = test::TestDescAndFn {
     desc: test::TestDesc {
-        name: test::StaticTestName("test_explicit_span_injection"),
+        name: test::StaticTestName("test_span_injection"),
         ignore: false,
         ignore_message: ::core::option::Option::None,
-        source_file: "winnow-grammar/tests/explicit_span_test.rs",
-        start_line: 24usize,
+        source_file: "winnow-grammar/tests/span_injection_test.rs",
+        start_line: 25usize,
         start_col: 4usize,
-        end_line: 24usize,
-        end_col: 32usize,
+        end_line: 25usize,
+        end_col: 23usize,
         compile_fail: false,
         no_run: false,
         should_panic: test::ShouldPanic::No,
@@ -200,44 +207,244 @@ pub const test_explicit_span_injection: test::TestDescAndFn = test::TestDescAndF
     },
     testfn: test::StaticTestFn(
         #[coverage(off)]
-        || test::assert_test_result(test_explicit_span_injection()),
+        || test::assert_test_result(test_span_injection()),
     ),
 };
-fn test_explicit_span_injection() {
-    let input = "  my_ident  ";
-    let input = LocatingSlice::new(input);
-    let result = ExplicitSpanTest::parse_custom_node().parse(input).unwrap();
-    match (&result.name, &"my_ident") {
-        (left_val, right_val) => {
-            if !(*left_val == *right_val) {
-                let kind = ::core::panicking::AssertKind::Eq;
-                ::core::panicking::assert_failed(
-                    kind,
-                    &*left_val,
-                    &*right_val,
-                    ::core::option::Option::None,
-                );
-            }
+fn test_span_injection() {
+    let input = "  42  ";
+    let expected = SpannedInt { val: 42, span: 2..4 };
+    SpanTest::parse_main().parse_test(input).assert_success_is(expected);
+}
+pub struct SpannedTuple {
+    pub a: u32,
+    pub b: u32,
+    pub span: std::ops::Range<usize>,
+}
+#[automatically_derived]
+impl ::core::fmt::Debug for SpannedTuple {
+    #[inline]
+    fn fmt(&self, f: &mut ::core::fmt::Formatter) -> ::core::fmt::Result {
+        ::core::fmt::Formatter::debug_struct_field3_finish(
+            f,
+            "SpannedTuple",
+            "a",
+            &self.a,
+            "b",
+            &self.b,
+            "span",
+            &&self.span,
+        )
+    }
+}
+#[automatically_derived]
+impl ::core::marker::StructuralPartialEq for SpannedTuple {}
+#[automatically_derived]
+impl ::core::cmp::PartialEq for SpannedTuple {
+    #[inline]
+    fn eq(&self, other: &SpannedTuple) -> bool {
+        self.a == other.a && self.b == other.b && self.span == other.span
+    }
+}
+#[automatically_derived]
+impl ::core::clone::Clone for SpannedTuple {
+    #[inline]
+    fn clone(&self) -> SpannedTuple {
+        SpannedTuple {
+            a: ::core::clone::Clone::clone(&self.a),
+            b: ::core::clone::Clone::clone(&self.b),
+            span: ::core::clone::Clone::clone(&self.span),
         }
-    };
-    match (&result.span, &(2..10)) {
-        (left_val, right_val) => {
-            if !(*left_val == *right_val) {
-                let kind = ::core::panicking::AssertKind::Eq;
-                ::core::panicking::assert_failed(
-                    kind,
-                    &*left_val,
-                    &*right_val,
-                    ::core::option::Option::None,
-                );
+    }
+}
+impl WithSpan<(u32, u32)> for SpannedTuple {
+    fn with_span(val: (u32, u32), span: std::ops::Range<usize>) -> Self {
+        Self { a: val.0, b: val.1, span }
+    }
+}
+#[allow(non_snake_case)]
+pub mod SpanTupleTest {
+    #![allow(unused_imports)]
+    #![allow(dead_code)]
+    use super::*;
+    use ::winnow::prelude::*;
+    use ::winnow::token::literal;
+    use ::winnow::combinator::{alt, repeat, opt, delimited, preceded};
+    #[allow(dead_code)]
+    fn WS<'a, S: std::fmt::Debug + Clone>(
+        input: &mut ::winnow_grammar::ParseInput<'a, S>,
+    ) -> ::winnow::Result<
+        (),
+        ::winnow::error::ErrMode<
+            ::winnow::error::InputError<::winnow_grammar::ParseInput<'a, S>>,
+        >,
+    > {
+        use ::winnow::Parser;
+        ::winnow::ascii::multispace0.parse_next(input).map(|_| ())
+    }
+    #[allow(dead_code)]
+    fn parse_main_inner<'a, S: std::fmt::Debug + Clone>(
+        input: &mut ::winnow_grammar::ParseInput<'a, S>,
+    ) -> ::winnow::Result<
+        SpannedTuple,
+        ::winnow::error::ErrMode<
+            ::winnow::error::InputError<::winnow_grammar::ParseInput<'a, S>>,
+        >,
+    > {
+        use ::winnow::Parser;
+        let mut parser = (|
+            input: &mut ::winnow_grammar::ParseInput<'a, S>,
+        | -> ::winnow::Result<
+            SpannedTuple,
+            ::winnow::error::ErrMode<
+                ::winnow::error::InputError<::winnow_grammar::ParseInput<'a, S>>,
+            >,
+        > {
+            use ::winnow::prelude::*;
+            {
+                let _ = WS(input)?;
+                let start = ::winnow::stream::Location::location(input);
+                let a = ::winnow::ascii::dec_uint::<
+                    ::winnow_grammar::ParseInput<'a, S>,
+                    u32,
+                    ::winnow::error::ErrMode<
+                        ::winnow::error::InputError<::winnow_grammar::ParseInput<'a, S>>,
+                    >,
+                >
+                    .parse_next(input)?;
+                let _ = WS(input)?;
+                let b = ::winnow::ascii::dec_uint::<
+                    ::winnow_grammar::ParseInput<'a, S>,
+                    u32,
+                    ::winnow::error::ErrMode<
+                        ::winnow::error::InputError<::winnow_grammar::ParseInput<'a, S>>,
+                    >,
+                >
+                    .parse_next(input)?;
+                let end = ::winnow::stream::Location::location(input);
+                #[allow(unused_variables)]
+                let _span = start..end;
+                Ok(::grammar_kit::WithSpan::with_span({ (a, b) }, _span))
             }
+        })
+            .context(::winnow::error::StrContext::Label("main"));
+        { parser.parse_next(input) }
+    }
+    pub fn parse_main<'a, S: std::fmt::Debug + Clone>() -> impl ::winnow::Parser<
+        ::winnow_grammar::ParseInput<'a, S>,
+        SpannedTuple,
+        ::winnow::error::InputError<::winnow_grammar::ParseInput<'a, S>>,
+    > {
+        move |
+            input: &mut ::winnow_grammar::ParseInput<'a, S>,
+        | -> ::winnow::Result<
+            SpannedTuple,
+            ::winnow::error::InputError<::winnow_grammar::ParseInput<'a, S>>,
+        > {
+            use ::winnow::error::ParserError;
+            match WS(input) {
+                Ok(v) => v,
+                Err(
+                    ::winnow::error::ErrMode::Backtrack(err)
+                    | ::winnow::error::ErrMode::Cut(err),
+                ) => return Err(err),
+                Err(::winnow::error::ErrMode::Incomplete(needed)) => {
+                    return Err(
+                        <::winnow::error::InputError<
+                            ::winnow_grammar::ParseInput<'a, S>,
+                        > as ParserError<
+                            ::winnow_grammar::ParseInput<'a, S>,
+                        >>::incomplete(input, needed),
+                    );
+                }
+            };
+            let result = match parse_main_inner(input) {
+                Ok(v) => v,
+                Err(
+                    ::winnow::error::ErrMode::Backtrack(err)
+                    | ::winnow::error::ErrMode::Cut(err),
+                ) => return Err(err),
+                Err(::winnow::error::ErrMode::Incomplete(needed)) => {
+                    return Err(
+                        <::winnow::error::InputError<
+                            ::winnow_grammar::ParseInput<'a, S>,
+                        > as ParserError<
+                            ::winnow_grammar::ParseInput<'a, S>,
+                        >>::incomplete(input, needed),
+                    );
+                }
+            };
+            match WS(input) {
+                Ok(v) => v,
+                Err(
+                    ::winnow::error::ErrMode::Backtrack(err)
+                    | ::winnow::error::ErrMode::Cut(err),
+                ) => return Err(err),
+                Err(::winnow::error::ErrMode::Incomplete(needed)) => {
+                    return Err(
+                        <::winnow::error::InputError<
+                            ::winnow_grammar::ParseInput<'a, S>,
+                        > as ParserError<
+                            ::winnow_grammar::ParseInput<'a, S>,
+                        >>::incomplete(input, needed),
+                    );
+                }
+            };
+            match ::winnow::combinator::eof.parse_next(input) {
+                Ok(v) => v,
+                Err(
+                    ::winnow::error::ErrMode::Backtrack(err)
+                    | ::winnow::error::ErrMode::Cut(err),
+                ) => return Err(err),
+                Err(::winnow::error::ErrMode::Incomplete(needed)) => {
+                    return Err(
+                        <::winnow::error::InputError<
+                            ::winnow_grammar::ParseInput<'a, S>,
+                        > as ParserError<
+                            ::winnow_grammar::ParseInput<'a, S>,
+                        >>::incomplete(input, needed),
+                    );
+                }
+            };
+            Ok(result)
         }
+    }
+}
+extern crate test;
+#[rustc_test_marker = "test_span_injection_tuple"]
+#[doc(hidden)]
+pub const test_span_injection_tuple: test::TestDescAndFn = test::TestDescAndFn {
+    desc: test::TestDesc {
+        name: test::StaticTestName("test_span_injection_tuple"),
+        ignore: false,
+        ignore_message: ::core::option::Option::None,
+        source_file: "winnow-grammar/tests/span_injection_test.rs",
+        start_line: 58usize,
+        start_col: 4usize,
+        end_line: 58usize,
+        end_col: 29usize,
+        compile_fail: false,
+        no_run: false,
+        should_panic: test::ShouldPanic::No,
+        test_type: test::TestType::IntegrationTest,
+    },
+    testfn: test::StaticTestFn(
+        #[coverage(off)]
+        || test::assert_test_result(test_span_injection_tuple()),
+    ),
+};
+fn test_span_injection_tuple() {
+    let input = " 10 20 ";
+    let expected = SpannedTuple {
+        a: 10,
+        b: 20,
+        span: 1..6,
     };
+    SpanTupleTest::parse_main().parse_test(input).assert_success_is(expected);
 }
 #[rustc_main]
 #[coverage(off)]
 #[doc(hidden)]
 pub fn main() -> () {
     extern crate test;
-    test::test_main_static(&[&test_explicit_span_injection])
+    test::test_main_static(&[&test_span_injection, &test_span_injection_tuple])
 }
